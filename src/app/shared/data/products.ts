1c4bba96c39208bc4d806e546c2b422a285b43ff
paul <pankajpaul0007@gmail.com>
2024-02-28 18:55:38 +0530

    first commit

---
+++ src/app/shared/data/products.ts
@@ -0,0 +1,60 @@
+export class productsDB {
+  static Product = [
+    {
+      id: 1,
+      created_by: {
+        name: 'Sajal Mia',
+        avatar: 'assets/images/avatar.png'
+      },
+      images: ['assets/images/products/hero.png'],
+      name: 'Industry marketing by subscription',
+      price: 1999.0,
+      rating: 4.8,
+      feedback: 3,
+      category: 'digitalization',
+      tags: ['Diesel', 'Hudson', 'Lee']
+    },
+    {
+      id: 2,
+      created_by: {
+        name: 'Sajal Mia',
+        avatar: 'assets/images/avatar.png'
+      },
+      images: ['assets/images/products/product1.jpeg'],
+      name: 'Potential analysis - digitization for manufacturing companies',
+      price: 3000.0,
+      rating: 4.8,
+      feedback: 3,
+      category: 'digitalization',
+      tags: ['Diesel', 'Hudson', 'Lee']
+    },
+    {
+      id: 3,
+      created_by: {
+        name: 'Sajal Mia',
+        avatar: 'assets/images/avatar.png'
+      },
+      images: ['assets/images/products/product2.jpeg'],
+      name: 'Task Management Scheduler',
+      price: 3000.0,
+      rating: 4.8,
+      feedback: 3,
+      category: 'digitalization',
+      tags: ['Diesel', 'Hudson', 'Lee']
+    },
+    {
+      id: 4,
+      created_by: {
+        name: 'Sajal Mia',
+        avatar: 'assets/images/avatar.png'
+      },
+      images: ['assets/images/products/product3.jpeg'],
+      name: 'Industry marketing by subscription',
+      price: 3000.0,
+      rating: 4.8,
+      feedback: 3,
+      category: 'digitalization',
+      tags: ['Diesel', 'Hudson', 'Lee']
+    }
+  ];
+}
